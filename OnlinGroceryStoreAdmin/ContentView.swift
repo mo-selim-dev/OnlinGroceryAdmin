dfd3f97032ab6dee91f98498a0850906cb2bcda8
Mohammed Selim <mo.selim.dev@gmail.com>
2025-02-27 16:46:12 +0200

    Initial Commit

---
+++ OnlinGroceryStoreAdmin/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  OnlinGroceryStoreAdmin
+//
+//  Created by Mohamed Selim on 27/02/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ OnlinGroceryStoreAdmin/OnlinGroceryStoreAdminApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnlinGroceryStoreAdminApp.swift
+//  OnlinGroceryStoreAdmin
+//
+//  Created by Mohamed Selim on 27/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct OnlinGroceryStoreAdminApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
